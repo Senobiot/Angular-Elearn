1acd63d480c2c1d5b8dbc81179d5823ad3fcb2b4
Aliaksandr Kulik <aliaksandr_kulik@epam.com>
2022-11-01 20:57:31 +0100

    made fixes according with remarks

--- src/app/features/course/course.component.ts
+++ src/app/features/course/course.component.ts
@@ -1,26 +1,35 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { faPencil, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
+
+interface Course {
+  editable?: boolean;
+  title?: string;
+  description?: string;
+  creationDate?: Date;
+  duration?: number;
+  authors?: string;
+}
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.scss']
 })
 export class CourseComponent implements OnInit {
+  @Input() course: Course = {};
   @Input() editable = true;
-  @Input() title: string = '';
-  @Input() description: string = '';
-  @Input() creationDate: Date = new Date();
-  @Input() duration: number = 0;
-  @Input() authors: string[] = [];
-
-  primaryIcon = faTrashCan;
-  secondaryIcon = faPencil;
 
   constructor() {
   }
 
   ngOnInit(): void {
+  }
+
+  formattedDuration(duration = this.course.duration || 0): string {
+    const hours = Math.trunc(duration / 60);
+    const minutes = duration % 60;
+    const ending = hours > 1 ? 'hours' : 'hour';
+
+    return `${hours}:${minutes} ${ending}`;
   }
 
 }
--- src/app/features/login/login.module.ts
+++ src/app/features/login/login.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from "./login.component";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [
     CommonModule
   ],
